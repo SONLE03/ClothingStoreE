4d1061106b2bbb95323d3e8b43149ab073e667b7
SONLE03 <sonle102003@gmail.com>
2024-06-06 03:10:09 +0700

    fix

--- src/main/java/com/sa/clothingstore/ClothingstoreApplication.java
+++ src/main/java/com/sa/clothingstore/ClothingstoreApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class git aClothingstoreApplication {
+public class ClothingstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClothingstoreApplication.class, args);
